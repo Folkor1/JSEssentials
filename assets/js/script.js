05c3a77ebe002a50337bef8e07748187d85d23bb
Folkor1 <igorvasiljevs@gmail.com>
2022-05-06 22:59:02 +0000

    Messing with concepts

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,55 @@
 /* name field validation */
 
+let quizButtons = document.getElementsByClassName("main-nav");
+let quizQuestion = document.getElementById("question-div");
+let quizGame = document.getElementById("quiz-div");
+let nameHeading = document.getElementById("name-div");
+let input = document.getElementById("name-input");
+let start = document.getElementById("start-button");
+
+let questions = [
+    "Question1",
+    "Question2",
+    "Question3",
+    "Question4",
+    "Question5",
+    "Question6",
+    "Question7",
+    "Question8",
+    "Question9",
+    "Question10",
+];
+
+let wrongAnswers = [
+    "wrong1",
+    "wrong2",
+    "wrong3",
+    "wrong4",
+    "wrong5",
+    "wrong6",
+    "wrong7",
+    "wrong8",
+    "wrong9",
+    "wrong10",
+]
+
+let randomQuestions = ["answer1", "answer2", 'answer3', "answer4"];
+
 function inputStart() {
-    if (input.value.match(/^[0-9A-Za-z]+$/) === null) {
-        alert("Enter your name in Alphanumeric please");
+    if (input.value.match(/^[0-9A-Za-z]+$/) !== null) {
+        input.style.display = "none";
+        start.style.display = "none";
+        nameHeading.style.display = "none";
+        quizGame.style.display = "block";
+        getData();
 } else {
-    window.location.href = 'quiz.html';
+    alert("Enter your name in Alphanumeric please");
     }
 }
 
-let input = document.getElementById("name-input");
-let start = document.getElementById("start-button");
-let nameField = document.getElementById("name-span");
+function getData() {
+    quizQuestion.innerText = questions[Math.floor(Math.random() * questions.length)];
+    
+}
+
 start.addEventListener('click', inputStart);
-
-
-/* insert name into leaderboard */
-
